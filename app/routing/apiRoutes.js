ec3040d6c3f823e95e11e3df9ec4d5dc6766dba4
h-Benitez13 <hbenitez3711@gmail.com>
2019-02-21 20:45:53 -0500

    unfinished

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -26,6 +26,15 @@ module.exports = function(app) {
           console.log(matchData[i]);
           for (var j = 0; j< 10; j++ ) {
             console.log(matchData[i].score);
+            // what was suppose to go through was two loops
+            // the first loop would be to go through each of the scores for my default data
+            // as this loop goes through, my 'userscore' would be equal to zero
+            // the next loop would go through each score in my default data and compare it to the user data input
+            // subtract each score and take the absolute value 
+            // once values are obtained
+            // then will take the total sum of all the differences and compare it to the variable set to 100
+            // i would then compare the total difference compared to the 100
+            // whatever score is closest to 100, that would be the match, which would then post to my data to upload in the modal
           }
 
 
